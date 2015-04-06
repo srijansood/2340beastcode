cdf0ee1988555acb7a127b67e0a3319f0d9dbc62
srijansood <srijansood@gmail.com>
2015-04-06 18:00:24 -0400

    Inspect MapsActivity

--- app/src/main/java/com/beastcode/MapsActivity.java
+++ app/src/main/java/com/beastcode/MapsActivity.java
@@ -14,7 +14,7 @@ public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
-    SharedPreferences prefs = null;
+    private SharedPreferences prefs = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +22,7 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
 
         prefs = this.getSharedPreferences("LatLng",MODE_PRIVATE);
-        prefs.edit().putInt("isClicked", 0).commit();
+        prefs.edit().putInt("isClicked", 0).apply();
         mMap.setMyLocationEnabled(true);
 //Check whether your preferences contains any values then we get those values
 //        if((prefs.contains("Lat")) && (prefs.contains("Lng")))
@@ -39,10 +39,10 @@ public class MapsActivity extends FragmentActivity {
             public void onMapClick(LatLng point) {
                 mMap.addMarker(new MarkerOptions().position(point));
 
-/* This code will save your location coordinates in SharedPrefrence when you click on the map and later you use it  */
-                prefs.edit().putString("Lat",String.valueOf(point.latitude)).commit();
-                prefs.edit().putString("Lng",String.valueOf(point.longitude)).commit();
-                prefs.edit().putInt("isClicked", 1).commit();
+/* This code will save your location coordinates in SharedPreference when you click on the map and later you use it  */
+                prefs.edit().putString("Lat",String.valueOf(point.latitude)).apply();
+                prefs.edit().putString("Lng",String.valueOf(point.longitude)).apply();
+                prefs.edit().putInt("isClicked", 1).apply();
                 finish();
 
             }
@@ -63,9 +63,7 @@ public class MapsActivity extends FragmentActivity {
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
+     * installed) and the map has not already been instantiated..  <p/>
      * If it isn't installed {@link SupportMapFragment} (and
      * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
      * install/update the Google Play services APK on their device.
@@ -83,19 +81,10 @@ public class MapsActivity extends FragmentActivity {
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
             // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
+//            if (mMap != null) {
+//                //setUpMap();
+//            }
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-    }
 }
